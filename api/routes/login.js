0aea09033d7750aad4f5c61166f8e1e1258c4717
sakerlucas <sakerlucas@gmail.com>
2022-01-25 15:32:08 +0100

    mise a mort des log

--- api/routes/login.js
+++ api/routes/login.js
@@ -5,7 +5,7 @@ const sql = require("../db.js");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.status(200).sendFile(path.resolve(__dirname + "/../public/login.html"))
 });
 
@@ -15,14 +15,13 @@ router.post('/', (req, res) => {
   let mdp = req.body.mdp
 
   sql.query('SELECT * FROM utilisateur WHERE mail = ?', [mail], (err, result) => {
-    console.log(result);
-      if (err) throw err
+    if (err) throw err
 
-      if ((result.length) && (bcrypt.compareSync(mdp, result[0].mdp)) && (mail == result[0].mail)) {
-          res.send('Connecté')
-      } else {
-          res.send('Mauvais pseudo ou mot de passe')
-      }
+    if ((result.length) && (bcrypt.compareSync(mdp, result[0].mdp)) && (mail == result[0].mail)) {
+      res.send('Connecté')
+    } else {
+      res.send('Mauvais pseudo ou mot de passe')
+    }
   })
 })
 
